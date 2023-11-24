9db15bbdd29338d3c72faed112decd9fb2c45623
Iván <ivarcass@gmail.com>
2023-11-24 14:05:50 +0100

    Metodos 3a clase

--- Producto.java
+++ Producto.java
@@ -1,10 +1,30 @@
 public class Producto {
-    
+
     private String nombre;
     private int precio;
-    
-    private Producto (String nombreProducto, int precioProducto) {
+
+    public Producto (String nombreProducto, int precioProducto) {
         nombre = nombreProducto;
         precio = precioProducto;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void imprimirDetallesProducto(){
+        System.out.println("El nombre del producto es " + nombre + ", y su precio es " + precio + "€");
     }
 }
