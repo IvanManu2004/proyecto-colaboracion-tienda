2d21d68c4c15f2e48a517957d4f9877ca883b798
IvanManu2004 <Ivanmanu2004@gmail.com>
2023-11-24 13:59:49 +0100

    Commit 1 con estructura de 3ª clase

---
+++ Producto.java
@@ -0,0 +1,10 @@
+public class Producto {
+    
+    private String nombre;
+    private int precio;
+    
+    private Producto (String nombreProducto, int precioProducto) {
+        nombre = nombreProducto;
+        precio = precioProducto;
+    }
+}
